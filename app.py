a67bc90e5b6da154cb78f7a2c645622854d0ad87
kacamata09 <muh.ansharazhari@gmail.com>
2022-09-18 21:09:13 +0700

    selesaikan kategori post juga tambahkan edit post dan hapus post dan edit beberapa yang lainnya

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
+import re
 from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from forms import TambahPost
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'isi random string aja atau buat generator secret key sendiri'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/dbku'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 dbku = SQLAlchemy(app)
+migrasidbku = Migrate(app, dbku)
 
 # ORM blog
 class Blog(dbku.Model):
     idPost = dbku.Column(dbku.Integer, primary_key=True)
     judulPost = dbku.Column(dbku.String(50), nullable=False)
     kontenPost = dbku.Column(dbku.String(2000))
+    kategoriPost = dbku.Column(dbku.String(50))
     tanggalPost = dbku.Column(dbku.DateTime, default=datetime.today())
     
 
 # jika terjadi error
 @app.errorhandler(404)
-def tidak_ditemukan():
-    return render_template('pencariangagal.html')
+def tidak_ditemukan(e):
+    return render_template('blog/pencariangagal.html'), 404
 
 
 
@@ -28,7 +33,7 @@ def tidak_ditemukan():
 @app.route('/')
 def index():
     
-    return render_template('index.html')
+    return render_template('index.html', titlepage='Home')
 
 # app untuk login, register
 @app.route('/login/')
@@ -39,36 +44,61 @@ def login():
 def register():
     return render_template('register.html')
 
-
 # app untuk blog
-@app.route('/tambahpost/', methods=['POST', 'GET'])
+@app.route('/post/tambahpost/', methods=['POST', 'GET'])
 def tambah_post():
     if request.method == 'POST':
         judul = request.form['judul']
         konten = request.form['konten']
-        tambahPost = Blog(judulPost=judul, kontenPost=konten)
+        kategori = request.form['kategori']
+        tambahPost = Blog(judulPost=judul, kontenPost=konten, kategoriPost = kategori)
         dbku.session.add(tambahPost)
         dbku.session.commit()
         return redirect(url_for('tambah_post'))
-    return render_template('blog/tambah_post.html', form=TambahPost())
+    
+    semua_post = Blog.query.all()
+    return render_template('blog/tambahpost.html', form=TambahPost(), titlepage='Tambah Post', semuapost=semua_post)
 
+@app.route('/post/editpost/<int:id>/', methods=['GET', 'POST'])
+def edit_post(id):
+    getPost = Blog.query.get_or_404(id)
+    if request.method == 'POST':
+        getPost.judulPost = request.form['judul']
+        getPost.kontenPost = request.form['konten']
+        getPost.kategoriPost = request.form['kategori']
+        dbku.session.commit()
+        return redirect(url_for('tambah_post'))
+    return render_template('blog/editpost.html', form=TambahPost(), post=getPost, titlepage='Edit Post')
+
+@app.route('/post/hapuspost/<int:id>/')
+def hapus_post(id):
+    getPost = Blog.query.get_or_404(id)
+    dbku.session.delete(getPost)
+    dbku.session.commit()
+    return redirect(url_for('tambah_post'))
+    
+
+# post blog
 @app.route('/post/')
 def semua_post():
     semua_post = Blog.query.all()
-    return render_template('blog/allpost.html', semuapost = semua_post)
+    return render_template('blog/allpost.html', semuapost = semua_post, titlepage='All Post')
 
 @app.route('/post/<kategori>/')
 def kategori_post(kategori):
-    semua_post = Blog.query.all()
-    return render_template('blog/kategoripost.html', semuapost = semua_post)
+    kategoripost = Blog.query.filter(Blog.kategoriPost.ilike(f'%{kategori}%') )
+    return render_template('blog/pencarianpost.html', kategori = kategoripost, titlepage=f'Kategori Post {kategori}')
+
+# ilike("%ganye%"
+@app.route('/post/halkategori/')
+def halaman_kategori():
+    return render_template('blog/halamankategori.html', titlepage='Kategori')
+
 
 @app.route('/post/<int:id>/')
 def detail_post(id):
-    try:
-        getPost = Blog.query.get_or_404(id)
-    except:
-        return redirect(url_for('tidak_ditemukan'))
-    return render_template('blog/detailpost.html', post=getPost)
+    getPost = Blog.query.get_or_404(id)
+    return render_template('blog/detailpost.html', post=getPost, titlepage=getPost.judulPost)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
 
 # buat form untuk tambah post
 class TambahPost(FlaskForm):
     judul = StringField('Judul Postingan', validators=[DataRequired()])
     konten = TextAreaField('Konten')
+    kategori = SelectField('Pilih Kategori', choices=('Berita', 'Viral', 'Teknologi'))
