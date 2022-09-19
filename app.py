b8041e1018ebe94881df7ce24b688e6679296db2
kacamata09 <muh.ansharazhari@gmail.com>
2022-09-19 21:31:44 +0700

    menambahkan fitur register, dan penambahan login dan register html. Tapi belum ada fitur loginnya, besok aja deh

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-import re
 from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from forms import TambahPost
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+
+# import forms
+from forms import TambahPost, Register
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'isi random string aja atau buat generator secret key sendiri'
@@ -20,6 +23,31 @@ class Blog(dbku.Model):
     kontenPost = dbku.Column(dbku.String(2000))
     kategoriPost = dbku.Column(dbku.String(50))
     tanggalPost = dbku.Column(dbku.DateTime, default=datetime.today())
+    
+class Pengguna(dbku.Model):
+    __tablename__ = 'flasklogin-pengguna'
+    
+    idPengguna = dbku.Column(dbku.Integer, primary_key=True)
+    namaLengkap = dbku.Column(dbku.String(255), nullable=False)
+    username = dbku.Column(dbku.String(50), nullable=False)
+    password_hash = dbku.Column(dbku.String(255), default='sandi123')
+    
+    # fungsi untuk hashing dan cek password
+    # fungsi password akan berubah menjadi properti atau variable kelas ini menggunakan decorator property
+    # tapi dengan raise attributeerror maka atribut tidak akan bisa diakses untuk melihat password
+    @property
+    def password(self):
+        raise AttributeError('Maaf tidak bisa melihat password hehe')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password, 'sha256')
+    
+    # @password.getter
+    def cek_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    
     
 
 # jika terjadi error
@@ -38,11 +66,30 @@ def index():
 # app untuk login, register
 @app.route('/login/')
 def login():
-    return render_template('login.html')
+    return render_template('login/login.html')
 
-@app.route('/register/')
+@app.route('/register/', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        nama = request.form['nama']
+        username = request.form['username']
+        sandi = request.form['password']
+        sandi_konfirmasi = request.form['password_konfirmasi']
+        sandi_hash = generate_password_hash(sandi, 'sha256')
+        tambah_pengguna = Pengguna(namaLengkap=nama, username=username, password_hash=sandi_hash)
+        dbku.session.add(tambah_pengguna)
+        dbku.session.commit()
+        return redirect(url_for('login'))
+        
+    return render_template('login/register.html', form=Register())
+
+@app.route('/admin/hapuspengguna/<int:id>')
+def hapus_user(id):
+    return render_template('login/hapususer.html')
+
+@app.route('/admin/editpengguna/<int:id>')
+def edit_user(id):
+    return render_template('login/edituser.html')
 
 # app untuk blog
 @app.route('/post/tambahpost/', methods=['POST', 'GET'])
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField
+from wtforms import StringField, TextAreaField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 
 # buat form untuk tambah post
@@ -7,3 +7,10 @@ class TambahPost(FlaskForm):
     judul = StringField('Judul Postingan', validators=[DataRequired()])
     konten = TextAreaField('Konten')
     kategori = SelectField('Pilih Kategori', choices=('Berita', 'Viral', 'Teknologi'))
+
+class Register(FlaskForm):
+    nama = StringField('Nama Lengkap', validators=[DataRequired()])
+    username = StringField('Nama Pengguna / Username', validators=[DataRequired()])
+    password = PasswordField('Kata Sandi Anda', validators=[DataRequired()])
+    password_konfirmasi = PasswordField('Masukkan Ulang Kata Sandi Anda', validators=[DataRequired()])
+    
